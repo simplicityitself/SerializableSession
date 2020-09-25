02ec4216cb71fa996a9dca1c251f0f5bb6e4ad9f
Dmitriy Drenkalyuk <dmitriy.drenkalyuk@secretescapes.com>
2020-09-25 12:26:46 +0100

    Updating event listener.

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,6 +1,8 @@
+import grails.util.Environment
 
-eventConfigureTomcat = {def tomcat ->
-  def valveClass = classLoader.loadClass("com.dawsonsystems.session.SessionTrackerValve")
-
-  tomcat.host.addValve(valveClass.newInstance(tomcat))
+eventConfigureTomcat = { def tomcat ->
+	if (Environment.current in [Environment.DEVELOPMENT, Environment.TEST]) {
+		def valveClass = this.class.classLoader.loadClass('com.flashsales.session.SessionTrackerValve')
+		tomcat.host.addValve(valveClass.newInstance(tomcat))
+	}
 }
