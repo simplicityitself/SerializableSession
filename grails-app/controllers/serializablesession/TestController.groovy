5ea22b1558095234d99bce0f4e81677b1399d2c7
Dmitriy Drenkalyuk <dmitriy.drenkalyuk@secretescapes.com>
2020-09-25 13:05:15 +0100

    Updating unit test to use mixins instead of deprecated test class hierarchy.

--- grails-app/controllers/serializablesession/TestController.groovy
+++ grails-app/controllers/serializablesession/TestController.groovy
@@ -2,25 +2,26 @@ package serializablesession
 
 class TestController {
 
-  def index = {
-    session["Simple"] = "Something serial"
-  }
+	def index = {
+		session["Simple"] = "Something serial"
+	}
 
-  def fail = {
-    session["Simple"] = new BadClass("WibbleWibbleWibbleWibbleWibbleWibbleWibbleWibble${System.currentTimeMillis()}")
-    session["Simple2"] = new BadClass("WibbleWibbleWibbleWibbleWibbleWibbleWibbleWibble${System.currentTimeMillis()}")
-    session["Simple3"] = new BadClass("WibbleWibbleWibbleWibbleWibbleWibbleWibbleWibble${System.currentTimeMillis()}")
-    session["Simple4"] = new BadClass("WibbleWibbleWibbleWibbleWibbleWibbleWibbleWibble${System.currentTimeMillis()}")
-    session["Simple4"] = new BadClass("WibbleWibbleWibbleWibbleWibbleWibbleWibbleWibble${System.currentTimeMillis()}")
-    session["Simple5"] = new BadClass("WibbleWibbleWibbleWibbleWibbleWibbleWibbleWibble${System.currentTimeMillis()}")
-    session["Simple6"] = new BadClass("WibbleWibbleWibbleWibbleWibbleWibbleWibbleWibble${System.currentTimeMillis()}")
-    session["Simple7"] = new BadClass("WibbleWibbleWibbleWibbleWibbleWibbleWibbleWibble${System.currentTimeMillis()}")
-  }
+	def fail = {
+		session["Simple"] = new BadClass("WibbleWibbleWibbleWibbleWibbleWibbleWibbleWibble${System.currentTimeMillis()}")
+		session["Simple2"] = new BadClass("WibbleWibbleWibbleWibbleWibbleWibbleWibbleWibble${System.currentTimeMillis()}")
+		session["Simple3"] = new BadClass("WibbleWibbleWibbleWibbleWibbleWibbleWibbleWibble${System.currentTimeMillis()}")
+		session["Simple4"] = new BadClass("WibbleWibbleWibbleWibbleWibbleWibbleWibbleWibble${System.currentTimeMillis()}")
+		session["Simple4"] = new BadClass("WibbleWibbleWibbleWibbleWibbleWibbleWibbleWibble${System.currentTimeMillis()}")
+		session["Simple5"] = new BadClass("WibbleWibbleWibbleWibbleWibbleWibbleWibbleWibble${System.currentTimeMillis()}")
+		session["Simple6"] = new BadClass("WibbleWibbleWibbleWibbleWibbleWibbleWibbleWibble${System.currentTimeMillis()}")
+		session["Simple7"] = new BadClass("WibbleWibbleWibbleWibbleWibbleWibbleWibbleWibble${System.currentTimeMillis()}")
+	}
 }
 
 class BadClass {
-  def someName
-  BadClass(String name) {
-    someName = name
-  }
+	def someName
+
+	BadClass(String name) {
+		someName = name
+	}
 }
