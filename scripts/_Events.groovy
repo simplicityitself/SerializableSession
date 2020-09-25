b71e1922351d1f89e8587fa5a2eccc8e57ca9cb3
Dmitriy Drenkalyuk <dmitriy.drenkalyuk@secretescapes.com>
2020-09-25 13:10:13 +0100

    Removing grails env condition.

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,8 +1,4 @@
-import grails.util.Environment
-
 eventConfigureTomcat = { def tomcat ->
-	if (Environment.current in [Environment.DEVELOPMENT, Environment.TEST]) {
-		def valveClass = this.class.classLoader.loadClass('com.flashsales.session.SessionTrackerValve')
-		tomcat.host.addValve(valveClass.newInstance(tomcat))
-	}
+	def valveClass = this.class.classLoader.loadClass('com.flashsales.session.SessionTrackerValve')
+	tomcat.host.addValve(valveClass.newInstance(tomcat))
 }
