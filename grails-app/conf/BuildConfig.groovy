61f13a55a7b4b1f0a914ac3c4c516d61e4a3bc75
Dmitriy Drenkalyuk <dmitriy.drenkalyuk@secretescapes.com>
2020-09-25 11:52:43 +0100

    Upgrading dependencies.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -3,24 +3,28 @@ grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 
 grails.project.dependency.resolution = {
-    inherits("global") {
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    repositories {
-        grailsPlugins()
-        grailsHome()
-        grailsCentral()
-        mavenCentral()
-    }
-    dependencies {
-      build 'org.apache.httpcomponents:httpclient:4.0.3', {
-        export=false
-      }
-    }
-    plugins {
-      build ":release:3.0.1", {
-        export=false
-      }
-      build ':tomcat:7.0.41'
-    }
+	inherits("global") {
+	}
+
+	log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+
+	repositories {
+		grailsPlugins()
+		grailsHome()
+		grailsCentral()
+		mavenCentral()
+	}
+
+	dependencies {
+		build 'org.apache.httpcomponents:httpclient:4.0.3', {
+			export = false
+		}
+	}
+
+	plugins {
+		build ":release:3.0.3", {
+			export = false
+		}
+		build ':tomcat:7.0.55.3'
+	}
 }
